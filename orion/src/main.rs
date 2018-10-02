b773b64c7f869eb4d1a2e69afc391ad820bf67c0
s1nus <connor@s1nus.com>
2018-10-01 23:54:04 -0400

    experimenting with HTTP requests and JSON... next step is adding concurrency. Need to follow along with the concurrent webserver in the book

--- orion/src/main.rs
+++ orion/src/main.rs
@@ -1,4 +1,9 @@
-use http::{ Request, Response};
+extern crate reqwest;
+extern crate serde_json;
+use serde_json::{Value, Error};
+use reqwest::get;
 fn main() {
-    println!("Hello, world!");
+    let body = reqwest::get("http://mysafeinfo.com/api/data?list=englishmonarchs&format=json").unwrap().text().unwrap(); 
+    let v : Value = serde_json::from_str(&body).unwrap();
+    println!("{}", v[0]["cty"]);
 }
